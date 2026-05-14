f5063e1ee8bb5c450527d4c652122cdb13ec1b27
jake-schmm <67937281+jake-schmm@users.noreply.github.com>
2026-05-14 18:40:10 -0400

    Set site URL and base path in astro config

--- astro-neon-portfolio/astro.config.mjs
+++ astro-neon-portfolio/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  site: 'https://jake-schmm.github.io',
+  base: '/MinistrySWPortfolio/',
   output: 'static',
   integrations: [mdx()]
 });
